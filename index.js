dde4f1d15cc417181b62ea4b57bf836911d315d7
endohedral <endohedral@skowron.uk>
2022-12-31 01:41:44 +0000

    Fixed it so you actually guess both nouns, not just one

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 var solutionNoun1 = "appetite";
 var solutionNoun2 = "ghost";
 
-// Set a flag to track whether the player has won the game
+// Set flags to track whether the player has revealed either noun or won the game
+var noun1Revealed = false;
+var noun2Revealed = false;
 var gameWon = false;
 
 // Add a function to handle the player's guess
@@ -19,24 +21,21 @@ function handleGuess(guess) {
 
   // Check if the player's guess matches either of the solution nouns
   if (guess === solutionNoun1 || guess === solutionNoun2) {
-    // If the guess is correct, reveal the solution noun and check if the game has been won
+    // If the guess is correct, reveal the solution noun and flag it as revealed
     if (guess === solutionNoun1) {
       $("#solution-noun-1").text(solutionNoun1);
+      noun1Revealed = true;
     } else if (guess === solutionNoun2) {
       $("#solution-noun-2").text(solutionNoun2);
+      noun2Revealed = true;
     }
 
     // Check if both nouns have been revealed
-    var noun1Revealed = $("#solution-noun-1").text() !== "???";
-    var noun2Revealed = $("#solution-noun-2").text() !== "???";
     if (noun1Revealed && noun2Revealed) {
       // If both nouns have been revealed, the player has won the game
       gameWon = true;
-      alert("Yay, you guessed one of the nouns :)");
+      alert("Yay, you guessed both nouns :)");
     }
-  } else {
-    // If the guess is incorrect, display a message
-    alert("Nope, that's not one of the nouns :( Guess again...");
   }
 }
 
@@ -44,10 +43,10 @@ function handleGuess(guess) {
 function promptGuess() {
   // If the game has not been won, prompt the player for a guess
   if (!gameWon) {
-    var playerGuess = prompt("Enter a noun:");
+    var playerGuess = prompt("Enter one noun:");
     handleGuess(playerGuess);
     // Schedule the next guess prompt
-    setTimeout(promptGuess, 500);
+    setTimeout(promptGuess, 750);
   }
 }
 
